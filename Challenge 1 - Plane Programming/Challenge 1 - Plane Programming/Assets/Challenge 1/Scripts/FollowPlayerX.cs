bf142835a1d07a6cf33e9a806776ad75c89d60ea
Ronan D <Ronan.d711@gmail.com>
2021-01-27 17:31:36 -0700

    Fixed the FollowPlayer script

---
+++ Challenge 1 - Plane Programming/Challenge 1 - Plane Programming/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    //calling the plane and storeing it
+    public GameObject plane;
+    //setting the offset
+    private Vector3 offset = new Vector3(0,5,-10);
+
+    // Update is called once per frame
+    void Update()
+    {
+        //have the camera follow the plane
+        transform.position = plane.transform.position + offset;
+    }
+}
