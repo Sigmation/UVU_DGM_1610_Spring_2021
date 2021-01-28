b81c179bef1d01bcf8ed18ad4b2d87028b15ca23
Ronan D <Ronan.d711@gmail.com>
2021-01-27 18:09:40 -0700

    Updated the PlayerControler script and made propeller spin

--- Challenge 1 - Plane Programming/Challenge 1 - Plane Programming/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Challenge 1 - Plane Programming/Challenge 1 - Plane Programming/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
-    public float verticalInput;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    //set the speed of the plane
+    private float speed = 0.5f;
+    //set the rotation speed of the plane
+    private float rotationSpeed = 0.0f;
+    //set up movement imput
+    private float verticalInput;
+    //set propeller speed
+    private float propellerSpeed = 1000.0f;
+    //declares the propeller
+    public GameObject propeller;
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
-        // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
-
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.left * rotationSpeed * verticalInput * Time.deltaTime);
+        //if else statment so only if the up or down key is pressed, will the plane rotate
+        if (verticalInput != 0)
+        {
+            rotationSpeed = 50.0f;
+        }
+        else {
+            rotationSpeed = 0.0f;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        //rotate ptopeller at a constant rate
+        propeller.transform.Rotate(Vector3.forward * propellerSpeed * Time.deltaTime);
     }
 }
