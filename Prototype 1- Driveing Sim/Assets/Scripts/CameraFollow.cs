49ae65c33b87c84f5c3fd1f491ab5d8de30733aa
Ronan D <Ronan.d711@gmail.com>
2021-01-29 01:10:09 -0700

    renamed the code to correct conventions

--- Prototype 1- Driveing Sim/Assets/Scripts/CameraFollow.cs
+++ Prototype 1- Driveing Sim/Assets/Scripts/CameraFollow.cs
@@ -7,12 +7,12 @@ public class CameraFollow : MonoBehaviour
     //calling the car and storing it
     public GameObject player;
     //setting an offset for the camera
-    private Vector3 Offset = new Vector3(0, 5, -10);
+    private Vector3 offset = new Vector3(0, 5, -10);
 
     // Update is called once per frame
     void Update()
     {
         //get the camera to follow the car
-        transform.position = player.transform.position + Offset;
+        transform.position = player.transform.position + offset;
     }
 }
--- Prototype 1- Driveing Sim/Assets/Scripts/Movement.cs
+++ Prototype 1- Driveing Sim/Assets/Scripts/Movement.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // setting the speed of the car
-    private float Speed = 30.0f;
+    private float speed = 30.0f;
     // setting the turn speed of the car
     private float turnSpeed = 60.0f;
     //horizontal movement
-    private float Hinput;
+    private float hinput;
     //forward movement
-    private float Finput;
+    private float finput;
 
     // Update is called once per frame
     void Update()
     {
         // gathers the imputs for the controls
-        Hinput = Input.GetAxis("Horizontal");
-        Finput = Input.GetAxis("Vertical");
+        hinput = Input.GetAxis("Horizontal");
+        finput = Input.GetAxis("Vertical");
         // makes the player car go forward and back
-        transform.Translate(Vector3.forward * Time.deltaTime * Speed * Finput);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * finput);
         // makes the player car go left and right
-        transform.Rotate(Vector3.up, turnSpeed * Hinput * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeed * hinput * Time.deltaTime);
     }
 }
