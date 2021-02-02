078d10f6021b7f44d2f0983255cbffa0e67fa03f
Ronan D <Ronan.d711@gmail.com>
2021-02-01 17:24:42 -0700

    Prototype 2 started

--- Prototype 2 - Animal Stampede/Prototype 2 Animal Stampede/Assets/Scripts/PlayerMovement.cs
+++ Prototype 2 - Animal Stampede/Prototype 2 Animal Stampede/Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //set move speed of the player
     public float speed = 20.0f;
+    //store the hinput
     public float hInput;
 
     // Update is called once per frame
     void Update()
     {
+        //declare hinput as the Horizontal Axis
         hInput = Input.GetAxis("Horizontal");
+        //setting left peramiters for movment
         if(transform.position.x < -10){
          transform.position = new Vector3(-10, transform.position.y, transform.position.z);
         }
+
+        //pushing the player on the x axis based on hinput
         transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
     }
 }
