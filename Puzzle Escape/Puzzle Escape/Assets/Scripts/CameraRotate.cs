9ffb7201e15ac0c3195fc963a953eb3f28ef01d7
Ronan D <Ronan.d711@gmail.com>
2021-02-07 15:01:55 -0700

    Continued work on personal game

---
+++ Puzzle Escape/Puzzle Escape/Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    //setting mouse y rotate
+    private float myinput;
+    // setting the rotation speed for the camera
+    private float turnSpeed = 200.0f;
+    // Update is called once per frame
+    void Update()
+    {
+    //setting imput for mouse y
+    myinput = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(Vector3.left, turnSpeed * myinput * Time.deltaTime);
+    }
+}
--- Puzzle Escape/Puzzle Escape/Assets/Scripts/PlayerMovement.cs
+++ Puzzle Escape/Puzzle Escape/Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // setting the movement speed for the player
-    public float speed = 30.0f;
+    private float speed = 5.0f;
+    //setting jump hight
+    private float jumpHight = 200.0f;
+    // setting the rotation speed for the player
+    private float turnSpeed = 200.0f;
     //setting horizontal movement
     private float hinput;
     //setting forward and backward movement
     private float finput;
-
+    //setting mouse x rotate
+    private float mxinput;
     // Update is called once per frame
     void Update()
     {
         // gathers the imputs for the controls
         hinput = Input.GetAxis("Horizontal");
         finput = Input.GetAxis("Vertical");
+        mxinput = Input.GetAxis("Mouse X");
         //moveing the player when imputs are pressed
         transform.Translate(Vector3.forward * Time.deltaTime * speed * finput);
         transform.Translate(Vector3.right * Time.deltaTime * speed * hinput);
-
+        //turning player with mouse
+        transform.Rotate(Vector3.up, turnSpeed * mxinput * Time.deltaTime);
+        //Button to Jump
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //}
     }
 }
