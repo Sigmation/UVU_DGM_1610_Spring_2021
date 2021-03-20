ea2613ddf83d2293b92787571f7165b779d54f48
Ronan D <Ronan.d711@gmail.com>
2021-03-20 03:54:23 -0600

    Added Player Control

--- Puzzle Escape/Puzzle Escape/Assets/Scripts/HideCursor.cs
+++ Puzzle Escape/Puzzle Escape/Assets/Scripts/HideCursor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class HideCursor : MonoBehaviour
 {
+    //Neeed a puase menu befroe adding
+
     // Start is called before the first frame update
     void Start()
     {
--- Puzzle Escape/Puzzle Escape/Assets/Scripts/PlayerMovement.cs
+++ Puzzle Escape/Puzzle Escape/Assets/Scripts/PlayerMovement.cs
@@ -4,33 +4,64 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // setting the movement speed for the player
     private float speed = 5.0f;
-    //setting jump hight
-    private float jumpHight = 200.0f;
-    // setting the rotation speed for the player
-    private float turnSpeed = 200.0f;
-    //setting horizontal movement
+    private float turnSpeed = 500.0f;
+    public float jump;
+    public float gravity;
+   
     private float hinput;
-    //setting forward and backward movement
     private float finput;
-    //setting mouse x rotate
-    private float mxinput;
+    private float rinput;
+    private float vinput;
+
+    public bool isOnGround = true;
+
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //declare player rigidbody
+        playerRb = GetComponent<Rigidbody>();
+        //set gravity
+        Physics.gravity *= gravity;
+       
+    }
+
     // Update is called once per frame
     void Update()
     {
-        // gathers the imputs for the controls
+        //set himput to horizontal movement
         hinput = Input.GetAxis("Horizontal");
+        //set fimput to vertical movement
         finput = Input.GetAxis("Vertical");
-        mxinput = Input.GetAxis("Mouse X");
-        //moveing the player when imputs are pressed
+        //set rimput to mouse x movement
+        rinput = Input.GetAxis("Mouse X");
+        //set vimput to mouse y movement
+        vinput = Input.GetAxis("Mouse Y");
+        //move forwad and back with Vertical movment
         transform.Translate(Vector3.forward * Time.deltaTime * speed * finput);
+        //move left and right with Horizontal movment
         transform.Translate(Vector3.right * Time.deltaTime * speed * hinput);
-        //turning player with mouse
-        transform.Rotate(Vector3.up, turnSpeed * mxinput * Time.deltaTime);
-        //Button to Jump
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //}
+        //rotate player with mouse x
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * rinput);
+
+        //jump if space pressed and on ground
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
+        {
+            playerRb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+            //set player on ground to false
+            isOnGround = false;
+        }
+    }
+    //OnCollisionEnter is called once per collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            //set player on ground to true
+            isOnGround = true;
+            Debug.Log("Grounded");
+        }
     }
 }
