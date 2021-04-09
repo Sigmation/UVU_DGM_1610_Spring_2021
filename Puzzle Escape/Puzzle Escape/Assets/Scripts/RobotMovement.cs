4832b7e3cdeefb03cf2dc8eb1d8ce4f16972ec9e
Ronan D <Ronan.d711@gmail.com>
2021-04-09 14:14:39 -0600

    Lab 4 complete

--- Puzzle Escape/Puzzle Escape/Assets/Scripts/RobotMovement.cs
+++ Puzzle Escape/Puzzle Escape/Assets/Scripts/RobotMovement.cs
@@ -2,39 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class RobotMovement : MonoBehaviour
 {
     private float speed = 5.0f;
     private float turnSpeed = 500.0f;
     public float jump = 8;
     public float gravity = 2;
-   
+
     private float hinput;
     private float finput;
     private float rinput;
     private float vinput;
 
-    public bool isOnGround = true;
-    public bool buttonPressed = false;
-    public bool doorOpen = false;
+    private PlayerMovement playerMovementScript;
 
-    private Rigidbody playerRb;
+    public bool isOnGround = true;
+    private Rigidbody robotRb;
 
     public GameObject spawnPoint;
-    public GameObject robot;
-    public GameObject playerCamera;
-    public GameObject robotCamera;
+    public GameObject door;
+    public GameObject button;
 
-
+    private Animator doorAnim;
+    private Animator buttonAnim;
 
     // Start is called before the first frame update
     void Start()
     {
-        //declare player rigidbody
-        playerRb = GetComponent<Rigidbody>();
-        //set gravity
-        Physics.gravity *= gravity;
-       
+        playerMovementScript = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        //declare robot rigidbody
+        robotRb = GetComponent<Rigidbody>();
+        doorAnim = door.GetComponent<Animator>();
+        buttonAnim = button.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -48,12 +47,9 @@ public class PlayerMovement : MonoBehaviour
         rinput = Input.GetAxis("Mouse X");
         //set vimput to mouse y movement
         vinput = Input.GetAxis("Mouse Y");
-        // if Robot control button is not pressed player can move
-        if (buttonPressed == false)
+
+        if (playerMovementScript.buttonPressed == true)
         {
-            // Swap camera to player camera
-            playerCamera.gameObject.SetActive(true);
-            robotCamera.gameObject.SetActive(false);
             //move forwad and back with Vertical movment
             transform.Translate(Vector3.forward * Time.deltaTime * speed * finput);
             //move left and right with Horizontal movment
@@ -61,52 +57,42 @@ public class PlayerMovement : MonoBehaviour
             //rotate player with mouse x
             transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * rinput);
         }
-        // if Robot control button is pressed robot can move
-        if (buttonPressed == true)
+        // jump if space pressed and on ground but for robot
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true && playerMovementScript.buttonPressed == true)
         {
-            // Swap camera to robot camera
-            robotCamera.gameObject.SetActive(true);
-            playerCamera.gameObject.SetActive(false);
-        }
-
-
-        // jump if space pressed and on ground
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true && buttonPressed == false)
-        {
-            playerRb.AddForce(Vector3.up * jump, ForceMode.Impulse);
-            //set player on ground to false
+            robotRb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+            //set robot on ground to false
             isOnGround = false;
         }
     }
     //OnCollisionEnter is called once per collision
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground") && buttonPressed == false)
+        if (collision.gameObject.CompareTag("Ground") && playerMovementScript.buttonPressed == true)
         {
-            //set player on ground to true
+            //set robot on ground to true
             isOnGround = true;
             Debug.Log("Grounded");
         }
+        // If robot touches button open the door
+        if (collision.gameObject.CompareTag("Button"))
+        {
+            playerMovementScript.buttonPressed = false;
+            playerMovementScript.doorOpen = true;
+            buttonAnim.SetBool("robotOnButton", true);
+            doorAnim.SetBool("doorIsOpen", true);
+            Debug.Log("RobotButton");
+        }
     }
-    // If player touches lava then they respawn
+    // If robot touches lava then they respawn
     private void OnTriggerEnter(Collider other)
     {
         // If player touches lava then they respawn
-        if (other.gameObject.CompareTag("Lava") && buttonPressed == false)
+        if (other.gameObject.CompareTag("Lava"))
         {
             transform.position = spawnPoint.transform.position;
             isOnGround = false;
             Debug.Log("Respawn");
-        }
-        
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && other.gameObject.CompareTag("Button") && buttonPressed == false && doorOpen == false)
-        {
-            buttonPressed = true;
-            Debug.Log("Button Pressed");
         }
     }
 }
