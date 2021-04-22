2928ca26ae51deeb0b254a5bc8fd8afe1e9772d6
Ronan D <Ronan.d711@gmail.com>
2021-04-21 22:06:07 -0600

    Completed 5.4 & Prototype 5

--- Prototype 5 - Clicky Mouse/Prototype 5 - Clicky Mouse/Assets/Scripts/GameManager.cs
+++ Prototype 5 - Clicky Mouse/Prototype 5 - Clicky Mouse/Assets/Scripts/GameManager.cs
@@ -21,13 +21,12 @@ public class GameManager : MonoBehaviour
 
     public Button resetButton;
 
+    public GameObject titleScreen;
+
     // Start is called before the first frame update
     void Start()
     {
-        score = 0;
-        scoreText.text = "Score: " + score;
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
+        
     }
 
     IEnumerator SpawnTarget()
@@ -44,7 +43,6 @@ public class GameManager : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-
         scoreText.text = "Score: " + score;
     }
 
@@ -58,5 +56,15 @@ public class GameManager : MonoBehaviour
     public void ResetGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        UpdateScore(0);
+        scoreText.text = "Score: " + score;
+        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
     }
 }
